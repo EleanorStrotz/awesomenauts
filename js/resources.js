09092e99d8b0c2690bd056c3314dc4b7b8607271
EleanorStrotz <EleanorStrotz@gmail.com>
2015-01-21 11:06:46 -0800

    video 4 coded and commented

--- js/resources.js
+++ js/resources.js
@@ -4,7 +4,10 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
+	 // loads our background tiles
+	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 // loads meta-tiles
+	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -15,7 +18,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+ 	 // loads level01
+ 	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
