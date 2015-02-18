da62909a13a6260deb49de5786abdcb601b5e070
EleanorStrotz <EleanorStrotz@gmail.com>
2015-02-18 11:27:54 -0800

    video 24 almost done

--- js/game.js
+++ js/game.js
@@ -5,7 +5,17 @@ var game = {
 	// an object where to store game information
 	data : {
 		// score
-		score : 0
+		score : 0,
+		enemyBaseHealth: 10,
+		playerBaseHealth: 10,
+		enemyCreepHealth: 10,
+		playerHealth: 10,
+		enemyCreepAttack: 1,
+		playerAttack: 1,
+		playerAttackTimer: 1000,
+		creepAttackTimer: 1000,
+		playerMoveSpeed: 5,
+		creepMoveSpeed: 5,
 	},
 	
 	
@@ -60,3 +70,6 @@ var game = {
 		me.state.change(me.state.PLAY);
 	}
 };
+
+
+//2:45 24
