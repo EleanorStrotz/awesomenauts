52fa27a601ed9b66f928f3a6e0fadcd0d4cf0601
EleanorStrotz <EleanorStrotz@gmail.com>
2015-02-23 12:14:29 -0800

    video 27 almost done

--- js/screens/title.js
+++ js/screens/title.js
@@ -3,7 +3,25 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		//adds title screen to the beggining of the game
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+	    //binds the enter key so that you can escape from the title screen
+	    //and go to the game, by pressing the enter key
+		me.input.bindKey(me.input.KEY.ENTER, "start");
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				this.font = new me.Font("Arial", 46, "white");
+			},
+
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
+				this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+			}
+		})));
+
+		this.handler = me.event.subscribe
 	},
 	
 	
@@ -14,3 +32,4 @@ game.TitleScreen = me.ScreenObject.extend({
 		; // TODO
 	}
 });
+//4:34
