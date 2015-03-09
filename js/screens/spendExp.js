8f9e6d73a5fd174d0262a32c5579c90e2cd63b1d
EleanorStrotz <EleanorStrotz@gmail.com>
2015-03-09 11:55:21 -0700

    video 42 coded and commented

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -4,15 +4,15 @@ game.SpendExp = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		//adds title screen to the beggining of the game
-		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
 	    
 
 		//adds the word awesomenauts to the game
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
-				this._super(me.Renderable, 'init', [270, 340, 250, 50]);
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 				this.font = new me.Font("Arial", 46, "white");
-				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+				//me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 			},
 
 			//function renderer tells the font size of the words in quotations
@@ -21,7 +21,7 @@ game.SpendExp = me.ScreenObject.extend({
 			}
 
 
-		}));
+		})));
 		
 	},
 	
--- js/screens/title.js
+++ js/screens/title.js
@@ -62,9 +62,8 @@ game.TitleScreen = me.ScreenObject.extend({
 				game.data.exp2 = me.save.exp2;
 				game.data.exp3 = me.save.exp3;
 				game.data.exp4 = me.save.exp4;
-				
 				me.input.releasePointerEvent('pointerdown', this);
-				me.state.change(me.state.PLAY);
+				me.state.change(me.state.SPENDEXP);
 			}
 		})));
 		
