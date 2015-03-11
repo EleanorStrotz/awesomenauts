e9439c86190b3a318fb4b506f1a0babb49550d47
EleanorStrotz <EleanorStrotz@gmail.com>
2015-03-11 11:12:19 -0700

    video 44 coded and commented

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -7,12 +7,11 @@ game.SpendExp = me.ScreenObject.extend({
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
 	    //binds the F1 key so that the player can increase their gold
 	    me.input.bindKey(me.input.KEY.F1, "F1");
-	    me.input.bindKey(me.input.KEY.F1, "F1");
-	    me.input.bindKey(me.input.KEY.F1, "F1");
-	    me.input.bindKey(me.input.KEY.F1, "F1");
-	    me.input.bindKey(me.input.KEY.F1, "F1");
-	    me.input.bindKey(me.input.KEY.F1, "F1");
-
+	    me.input.bindKey(me.input.KEY.F2, "F2");
+	    me.input.bindKey(me.input.KEY.F3, "F3");
+	    me.input.bindKey(me.input.KEY.F4, "F4");
+	    me.input.bindKey(me.input.KEY.F5, "F5");
+	
 		//adds the word awesomenauts to the game
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
@@ -33,6 +32,20 @@ game.SpendExp = me.ScreenObject.extend({
 
 
 		})));
+	// lets the key bind work
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
+			if(action === "F1"){
+
+			}else if(action === "F2"){
+
+			}else if(action === "F3"){
+				
+			}else if(action === "F4"){
+				
+			}else if(action === "F5"){
+				me.state.change(me.state.PLAY);
+			}
+		});
 		
 	},
 	
@@ -41,7 +54,13 @@ game.SpendExp = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-	
+	    //binds the F1 key so that the player can increase their gold
+	    me.input.unbindKey(me.input.KEY.F1, "F1");
+	    me.input.unbindKey(me.input.KEY.F2, "F2");
+	    me.input.unbindKey(me.input.KEY.F3, "F3");
+	    me.input.unbindKey(me.input.KEY.F4, "F4");
+	    me.input.unbindKey(me.input.KEY.F5, "F5");
+	    me.event.unsubscribe(this.handler);
 	}
 });
 // video 44, 6:08
