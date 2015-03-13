f52957ed203e71efd3e25c76a46dd36433a098af
EleanorStrotz <EleanorStrotz@gmail.com>
2015-03-13 11:03:43 -0700

    video 47 coded and commented

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -131,12 +131,18 @@ game.SpendGold = Object.extend({
 		this.buying = true;
 		me.state.pause(me.state.PLAY);
 		game.data.pausePos = me.game.viewport.localToWorld(0, 0);
-		game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'))
+		game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+		game.data.buyscreen.updateWhenPaused = true;
+		game.data.buyscreen.setOpacity(0.8);
+		me.game.world.addChild(game.data.buyscreen, 34);
+		game.data.player.body.setVelocity(0, 0);
 	},
 
 	stopBuying: function(){
 		this.buying = false;
 		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+		me.game.world.removeChild(game.data.buyscreen);
 	}
 
 	//6: 23
